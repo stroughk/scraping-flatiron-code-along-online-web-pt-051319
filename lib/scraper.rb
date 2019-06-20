0af99b0a9f48bd21025a153a86b74ac47878ca2d
Kateryna Strough <katerynastrough@hotmail.com>
2019-06-20 01:42:57 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -31,6 +31,17 @@ class Scraper
     end
   end
   
+   def get_page
+ 
+    Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+ 
+    #doc.css(".post").each do |post|
+      #course = Course.new
+      #course.title = post.css("h2").text
+      #course.schedule = post.css(".date").text
+      #course.description = post.css("p").text
+    #end
+  end
   
   
 end
